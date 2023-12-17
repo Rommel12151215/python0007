ed6c10f284ba409a52f9c89fbcc3f747ac55c037
Rommel <karlsrommel255@gmail.com>
2023-12-17 15:24:46 -0600

    Python0007

---
+++ main.py
@@ -0,0 +1,17 @@
+myName = input("What's your name?: ")
+if myName == "David":
+    print("Welcome Dude!")
+    print("You're just the baldest dude I've ever seen")
+else:
+    print("Who on earth are you?!")
+
+print()
+choice = input("Enter choice(1/2/3/4):")
+if choice == '1':
+  print("youre dead")
+elif choice == '2':
+  print("Congratulations, you have won the game")
+elif choice == '3':
+  print("Youre rich, yea")
+else:
+  print("You get was married")
